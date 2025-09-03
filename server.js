595f311a4e577310fa506d181ee3102fa4f1bde7
Tristan Hunt <tristan@ordr.io>
2025-09-03 17:56:01 -0600

    Allowing requests from the react frontend...

--- server.js
+++ server.js
@@ -11,7 +11,39 @@ import getPlantsData from './data/plants.js'
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:5173'
+];
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+
+    console.log("RECEIVED REQUEST!")
+    console.log("headers:")
+    console.log(req.headers);
+
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+
+        res.header('Access-Control-Allow-Origin', origin);
+
+    }
+
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+
+        return res.status(200).end();
+
+    }
+
+    next();
+
+});
 
 app.get('/plants', (req, res) => {
     const plants = getPlantsData()
